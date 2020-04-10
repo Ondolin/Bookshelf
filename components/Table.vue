7a2a61849f135a1dfbfc76277e16ea6af688b1b1
Caesar0815 <gingermk007@gmail.com>
2020-04-11 01:50:35 +0200

    added Table.vue added Navbar.vue

---
+++ components/Table.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="field">
+      <div class="control">
+        <input class="input is-primary" type="text" placeholder="Primary input">
+      </div>
+    </div>
+    <table class="table is-h-centerd">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Teile</th>
+          <th>ISBN</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="book in books">
+          <td>{{book.name}}</td>
+          <td>{{parseArray(book.teile)}}</td>
+          <td>{{book.isbn}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  import { getBooks } from '../plugins/API'
+
+  export default {
+    name: 'Table',
+    data() {
+      return {
+        books: ""
+      }
+    },
+    methods: {
+      parseArray(teile) {
+        let out = "";
+        for (let i = 0; i < teile.length; i++) {
+          if (out !== "") {
+            out += ", ";
+          }
+          out += teile[i];
+        }
+        return out;
+      }
+    },
+    created: function () {
+      this.books = getBooks()
+    }
+  }
+</script>
+
+
+<style scoped>
+  .is-h-centerd {
+    margin: auto;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="container">
-    Test
+  <div>
+    <Navbar></Navbar>
+    <div class="columns">
+      <div class="column">
+        <Table></Table>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+  import Table from '../components/Table'
+  import Navbar from '../components/Navbar'
+  import { getBooks } from '../plugins/API'
+
+  export default {
+    components: { Table, Navbar },
+    methods: {
+      getBooksFromAPI() {
+        return getBooks().stringify()
+      }
+    }
+  }
+</script>
