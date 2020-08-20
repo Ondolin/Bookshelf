11fc23145286e3a3fda6b8400be2f4acbdd7d702
Caesar0815 <gingermk007@gmail.com>
2020-08-20 17:35:16 +0200

    quagga test

---
+++ firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAcQHJEE3s6ejKGuZgju8bB6D2ldzEKdd0",
+  authDomain: "bookshelf-941cd.firebaseapp.com",
+  databaseURL: "https://bookshelf-941cd.firebaseio.com",
+  projectId: "bookshelf-941cd",
+  storageBucket: "bookshelf-941cd.appspot.com",
+  messagingSenderId: "290864702088",
+  appId: "1:290864702088:web:edc7794a92fad4937eb53d"
+};
